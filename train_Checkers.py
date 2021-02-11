69cb065f2cf1cde507bba026276f214bb4e11f24
AlexMGitHub <AlexMGitHub@gmail.com>
2021-02-11 11:56:44 -0500

    Iteration 9

--- train_Checkers.py
+++ train_Checkers.py
@@ -35,11 +35,11 @@
 """
 
 # %% Set training pipeline parameters
-TRAINING_ITERATION = 8 # Current training iteration
+TRAINING_ITERATION = 9 # Current training iteration
 # NN_FN required if TRAINING_ITERATION > 0 and SELFPLAY = TRUE
-NN_FN = 'data/model/Checkers_Model8_09-Feb-2021(18:38:37).h5'
+NN_FN = 'data/model/Checkers_Model9_11-Feb-2021(00:07:22).h5'
 # NEW_NN_FN required if TRAINING = FALSE and EVALUATION = TRUE
-NEW_NN_FN = 'data/model/Checkers_Model8_09-Feb-2021(18:38:37).h5'
+NEW_NN_FN = 'data/model/Checkers_Model9_11-Feb-2021(00:07:22).h5'
 SELFPLAY = True            # If True self-play phase will be executed
 TRAINING = False            # If True training phase will be executed
 EVALUATION = False          # If True evaluation phase will be executed
